fecf4c21c4af222f9d8c9fd9737b3d37ba78ebd5
Ryan Nenninger <rnenny@gmail.com>
2019-12-12 14:05:09 -0500

    Cleaned up code, added code backup, started mapping keys

--- code.py
+++ code.py
@@ -1,48 +1,35 @@
-# The MIT License (MIT)
-#
-# Copyright (c) 2017 Scott Shawcroft for Adafruit Industries
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-#adafruit_irremote
-====================================================
 
 import time
 import board
-import pulseio
 import digitalio
 from digitalio import DigitalInOut, Direction, Pull
-import adafruit_irremote
+from analogio import AnalogIn
 import adafruit_matrixkeypad
+import adafruit_irremote
+import pulseio
 
-# --------------------------------------------------------------------
-# RGB LED code section. See schematic for pin usage
-# Red - D9
-# Green - D10
-# Blue - D5
-# NEEDED TO INITILIZED PIN VALUES OR WONKY STUFF HAPPENS
-# CircuitPython takes care of active low pins interanally?
-# In this case False = False, as it would on active high pins
+# -----------------------------------------------------------------
+# State machine using dictonary definitions, as Python does not have switch statements
+"""
+states:
+0 - Idle
+1 - Battery CHeck
+2 - Device 1
+3 - Device 2
+4 - Device 3
+5 - Device Programming
+"""
 
+# -----------------------------------------------------------------
 
-red = DigitalInOut(board.D9)
-red.direction = Direction.OUTPUT
-red.value = False
+""" Global Variables """
+# state of program
+programRunning = True
+
+#red = DigitalInOut(board.D9)
+pwmred = pulseio.PWMOut(board.D9, frequency=60, duty_cycle=30)
+#red.direction = Direction.OUTPUT
+# pwmred.value = False
 
 green = DigitalInOut(board.D10)
 green.direction = Direction.OUTPUT
@@ -55,38 +42,29 @@ blue.value = False
 # Status / Heartbeat LED init
 led = DigitalInOut(board.D13)
 led.direction = Direction.OUTPUT
-# led.value = True
-# --------------------------------------------------------------------
-# Init IR recievers
-# ir_recieve1 = DigitalInOut(board.D12)
-# ir_recieve1.direction = Direction.INPUT
-#ir_recieve1.value = 0
-
-#ir_recieve2 = DigitalInOut(board.D13)
-#ir_recieve2.direction =  Direction.INPUT
-#ir_recieve2.value = 0
-
-# --------------------------------------------------------------------
-# mfet = DigitalInOut(board.D11)
-# mfet.direction = Direction.OUTPUT
-# mfet.value = 1
 
 
-#I think the diodes are in backwards? (Need verification)
+# I think the diodes are in backwards? (Need verification)
 # Swapped rows and columms
 # only works when rows and colums are swapped
 # I think I just misunderstood what constitues a row and a column
-# Anyways, this works 
+# Anyways, this works
 
+
+# this will be the global state for the remote
+# handles the logic for what the remote does depending on user interaction
+deviceState = 0
+
+# dictionary for keypresses
 key_dict = {}
 
 # Row D6 is now enabled/initialized yet
-rows = [digitalio.DigitalInOut(x) for x in (board.A0, board.A1, board.A2, board.A3, board.D6)]
-cols = [digitalio.DigitalInOut(x) for x in (board.A4, board.A5)]
+rows = [digitalio.DigitalInOut(pin) for pin in (board.A0, board.A1, board.A2, board.A3, board.D6)]
+cols = [digitalio.DigitalInOut(pin) for pin in (board.A4, board.A5)]
 
 keys = [
-    ["Power", "Volume+", "Volume-", "F1", "F3"],
-    ["Source", "Channel+", "Channel-", "F2",  "F4"],
+    ["power", "volume+", "volume-", "f1", "f3"],
+    ["source", "channel+", "channel-", "f2", "f4"],
 ]
 
 
@@ -96,53 +74,24 @@ keyPressed = False
 # IR setup
 pulsein = pulseio.PulseIn(board.D12, maxlen=120, idle_state=True)
 decoder = adafruit_irremote.GenericDecode()
-received_code = bytearray(4) # size must match what you are decoding! for NEC use 4
+
+# size must match what you are decoding! for NEC use 4
+received_code = bytearray(4)
 
 # Docs for adafruit_irremote: (https://circuitpython.readthedocs.io/projects/irremote/en/latest/api.html#implementation-notes)
 # Article for IR Apple Remote Code: https://www.hackster.io/BlitzCityDIY/circuit-python-ir-remote-for-apple-tv-e97ea0
 # IR Test Code (Apple Remote): https://github.com/BlitzCityDIY/Circuit-Python-Apple-TV-IR-Remote/blob/master/ciruitPython_appleTv_IR-Remote
 # pwm out test
-OKAY = bytearray(b'\x88\x1e\xc5 ') #decoded [136, 30, 197, 32]
+OKAY = bytearray(b'\x88\x1e\xc5 ')  # decoded [136, 30, 197, 32]
 remote = adafruit_irremote.GenericTransmit((9050, 4460), (550, 1650), (570, 575), 575)
-pwm = pulseio.PWMOut(D11, frequency = 38000, duty_cycle = 2 ** 15)
+pwm = pulseio.PWMOut(board.D11, frequency=38000, duty_cycle=2 ** 15)
 
-while True:
-    # Blink LED
-    led.value = True
-    time.sleep(0.2)
-    led.value = False
-    time.sleep(0.2)
+# ----------------------------------------------------------------------------------
+""" Functions """
 
 
-    """ Test IR Transmit """
-    # test_pulse = pulseio.PulseOut(pwm)
-    # remote.transmit(test_pulse, OKAY)
-    # print("Sent Test IR Signal!", OKAY)
-    # time.sleep(0.2)
-
-
-
-    """ Test IR Readings """
-    # pulses = decoder.read_pulses(pulsein, blocking=False)
-    # print(pulses)
-
-    # if pulses:
-    #     print("Heard", len(pulses), "Pulses:", pulses)
-        
-    #     try:
-    #         # Attempt to convert received pulses into numbers
-    #         received_code = decoder.decode_bits(pulses)
-    #         print("NEC Infrared code received: ", received_code)
-
-    #     except adafruit_irremote.IRNECRepeatException:
-    #         # We got an unusual short code, probably a 'repeat' signal
-    #         print("NEC repeat!")
-    #         continue
-    #     except adafruit_irremote.IRDecodeException as e:
-    #         # Something got distorted or maybe its not an NEC-type remote?
-    #         print("Failed to decode: ", e.args)
-    #         continue
- 
+def check_keypresses():
+    global keypad, keyPressed, key_dict, deviceState
 
     # this will be a list of returned keys'; [..., ...]
     keys = keypad.pressed_keys
@@ -152,20 +101,188 @@ while True:
         keyPressed = True
 
         for key in keys:
+            # handle reset of deviceState for 'source' key in device_state_3()
+            if key == "source":
+                deviceState += 1
+
             if key not in key_dict:
                 key_dict.update({key: 0})
             else:
                 key_dict.update({key: key_dict.get(key) + 1})
 
         print(key_dict)
-        # print("Pressed: ", keys)
 
     time.sleep(0.015)
 
     if len(keys) < 1:
         if len(key_dict) > 0:
             key_dict.clear()
+
         keyPressed = False
 
 
-print("Got here because exception or end of program")
+def test_ir_transmit():
+    global remote, OKAY, pwm
+
+    test_pulse = pulseio.PulseOut(pwm)
+    remote.transmit(test_pulse, OKAY)
+
+    print("Sent Test IR Signal!", OKAY)
+    time.sleep(0.2)
+
+
+def test_ir_receive():
+    global decoder, pulsein
+
+    pulses = decoder.read_pulses(pulsein, blocking=False)
+    print(pulses)
+
+    if pulses:
+        print("Heard", len(pulses), "Pulses:", pulses)
+
+        try:
+            # Attempt to convert received pulses into numbers
+            received_code = decoder.decode_bits(pulses)
+            print("NEC Infrared code received: ", received_code)
+
+        except adafruit_irremote.IRNECRepeatException:
+            # We got an unusual short code, probably a 'repeat' signal
+            print("NEC repeat!")
+            return
+
+        except adafruit_irremote.IRDecodeException as e:
+            # Something got distorted or maybe its not an NEC-type remote?
+            print("Failed to decode: ", e.args)
+            return
+
+
+
+# function converts value from a pin to a voltage between 0-3.3V
+def get_voltage(pin):
+    return (pin.value * 3.3) / 65536
+
+
+def continuous_updates():
+    check_keypresses()
+
+
+# Battery check
+def battery_check_state():
+    global pwmred, blue, green, rows
+
+    ledBlinkCount = 0
+    
+    # deinit A0 pin from keypad temp
+    rows[0].deinit()
+    time.sleep(0.25)
+
+    battery_voltage_pin = AnalogIn(board.A0)
+    battery_voltage = get_voltage(battery_voltage_pin)
+
+    print("Battery Check Voltage: ", battery_voltage)
+
+    if battery_voltage < 2:
+        while ledBlinkCount < 5:
+            pwmred.duty_cycle = 6000
+            time.sleep(0.25)
+            pwmred.duty_cycle = 0
+
+            # blue.value = True
+            # time.sleep(0.25)
+            # blue.value = False
+
+            # green.value = True
+            # time.sleep(0.25)
+            # green.value = False
+            
+            ledBlinkCount += 1
+
+    battery_voltage_pin.deinit()
+
+    rows[0] = digitalio.DigitalInOut(board.A0)
+
+def idle_state():
+    # check from inputs from all other states
+    # will be polling loop for global vars
+    battery_check_state()
+    pass
+
+
+def device_1_state():
+    global deviceState
+
+    test_ir_transmit_count = 10
+
+    while test_ir_transmit_count > 0:
+        test_ir_transmit()
+        test_ir_transmit_count -= 1
+
+    deviceState = 0    
+
+
+        
+
+def device_2_state():
+
+    pass
+
+
+def device_3_state():
+
+    pass
+
+
+def device_programming_state():
+
+    pass
+
+
+# handles state of program and device
+# this main loop can return based on the 'deviceState' global variable or either
+# in each separate state can set the programRunning variable false to stop the program
+
+
+while programRunning:
+
+    try:
+        # put any code that will run regardless of deviceState in this method
+        continuous_updates()
+
+        if deviceState == 0:
+            idle_state()
+
+        elif deviceState == 1:
+            battery_check_state()
+
+        elif deviceState == 2:
+            device_1_state()
+
+        elif deviceState == 3:
+            device_2_state()
+
+        elif deviceState == 4:
+            device_3_state()
+
+        elif deviceState == 5:
+            device_programming_state()
+
+    except KeyboardInterrupt as keyexcept:
+        print(keyexcept)
+
+    except() as ex:
+        print(ex)
+
+# Status LED setup
+# Onboard LED on D13 is always on, and cannot be turned off --
+#        This was wrong, we had a bad ground
+# However, it may be pulsed using the code below
+#        It still can work
+
+# led = DigitalInOut(board.D13)
+# led.direction = Direction.OUTPUT
+# led.value = False
+# while True:
+#     led.value = True
+#     time.sleep(0.4)
+#     led.value = False
+#     time.sleep(0.4)
---
+++ code_backup.py
@@ -0,0 +1,171 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2017 Scott Shawcroft for Adafruit Industries
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#adafruit_irremote
+#====================================================
+
+import time
+import board
+import pulseio
+import digitalio
+from digitalio import DigitalInOut, Direction, Pull
+import adafruit_irremote
+import adafruit_matrixkeypad
+
+# --------------------------------------------------------------------
+# RGB LED code section. See schematic for pin usage
+# Red - D9
+# Green - D10
+# Blue - D5
+# NEEDED TO INITILIZED PIN VALUES OR WONKY STUFF HAPPENS
+# CircuitPython takes care of active low pins interanally?
+# In this case False = False, as it would on active high pins
+
+
+red = DigitalInOut(board.D9)
+red.direction = Direction.OUTPUT
+red.value = False
+
+green = DigitalInOut(board.D10)
+green.direction = Direction.OUTPUT
+green.value = False
+
+blue = DigitalInOut(board.D5)
+blue.direction = Direction.OUTPUT
+blue.value = False
+
+# Status / Heartbeat LED init
+led = DigitalInOut(board.D13)
+led.direction = Direction.OUTPUT
+# led.value = True
+# --------------------------------------------------------------------
+# Init IR recievers
+# ir_recieve1 = DigitalInOut(board.D12)
+# ir_recieve1.direction = Direction.INPUT
+#ir_recieve1.value = 0
+
+#ir_recieve2 = DigitalInOut(board.D13)
+#ir_recieve2.direction =  Direction.INPUT
+#ir_recieve2.value = 0
+
+# --------------------------------------------------------------------
+mfet = DigitalInOut(board.D11)
+mfet.direction = Direction.OUTPUT
+mfet.value = 1
+
+
+#I think the diodes are in backwards? (Need verification)
+# Swapped rows and columms
+# only works when rows and colums are swapped
+# I think I just misunderstood what constitues a row and a column
+# Anyways, this works 
+
+key_dict = {}
+
+# Row D6 is now enabled/initialized yet
+rows = [digitalio.DigitalInOut(x) for x in (board.A0, board.A1, board.A2, board.A3, board.D6)]
+cols = [digitalio.DigitalInOut(x) for x in (board.A4, board.A5)]
+
+keys = [
+    ["Power", "Volume+", "Volume-", "F1", "F3"],
+    ["Source", "Channel+", "Channel-", "F2",  "F4"],
+]
+
+
+keypad = adafruit_matrixkeypad.Matrix_Keypad(cols, rows, keys)
+keyPressed = False
+
+# IR setup
+pulsein = pulseio.PulseIn(board.D12, maxlen=120, idle_state=True)
+decoder = adafruit_irremote.GenericDecode()
+received_code = bytearray(4) # size must match what you are decoding! for NEC use 4
+
+# Docs for adafruit_irremote: (https://circuitpython.readthedocs.io/projects/irremote/en/latest/api.html#implementation-notes)
+# Article for IR Apple Remote Code: https://www.hackster.io/BlitzCityDIY/circuit-python-ir-remote-for-apple-tv-e97ea0
+# IR Test Code (Apple Remote): https://github.com/BlitzCityDIY/Circuit-Python-Apple-TV-IR-Remote/blob/master/ciruitPython_appleTv_IR-Remote
+# pwm out test
+OKAY = bytearray(b'\x88\x1e\xc5 ') #decoded [136, 30, 197, 32]
+remote = adafruit_irremote.GenericTransmit((9050, 4460), (550, 1650), (570, 575), 575)
+pwm = pulseio.PWMOut(board.D9, frequency = 38000, duty_cycle = 2 ** 15)
+
+while True:
+    # Blink LED
+    led.value = True
+    time.sleep(0.2)
+    led.value = False
+    time.sleep(0.2)
+
+
+    """ Test IR Transmit """
+    # test_pulse = pulseio.PulseOut(pwm)
+    # remote.transmit(test_pulse, OKAY)
+    # print("Sent Test IR Signal!", OKAY)
+    # time.sleep(0.2)
+
+
+
+    """ Test IR Readings """
+    # pulses = decoder.read_pulses(pulsein, blocking=False)
+    # print(pulses)
+
+    # if pulses:
+    #     print("Heard", len(pulses), "Pulses:", pulses)
+        
+    #     try:
+    #         # Attempt to convert received pulses into numbers
+    #         received_code = decoder.decode_bits(pulses)
+    #         print("NEC Infrared code received: ", received_code)
+
+    #     except adafruit_irremote.IRNECRepeatException:
+    #         # We got an unusual short code, probably a 'repeat' signal
+    #         print("NEC repeat!")
+    #         continue
+    #     except adafruit_irremote.IRDecodeException as e:
+    #         # Something got distorted or maybe its not an NEC-type remote?
+    #         print("Failed to decode: ", e.args)
+    #         continue
+ 
+
+    # this will be a list of returned keys'; [..., ...]
+    keys = keypad.pressed_keys
+
+    # if we have any keys, then loop over the key names
+    if keys:
+        keyPressed = True
+
+        for key in keys:
+            if key not in key_dict:
+                key_dict.update({key: 0})
+            else:
+                key_dict.update({key: key_dict.get(key) + 1})
+
+        print(key_dict)
+        # print("Pressed: ", keys)
+
+    time.sleep(0.015)
+
+    if len(keys) < 1:
+        if len(key_dict) > 0:
+            key_dict.clear()
+        keyPressed = False
+
+
+print("Got here because exception or end of program")
